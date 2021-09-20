8ba6ae78957b99572d53522e22d6d540ba7fb582
Hank Shen <swhbox@foxmail.com>
2021-09-20 20:00:15 +0800

    improved

---
+++ forms_test.go
@@ -0,0 +1,71 @@
+package forms
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/coscms/forms/config"
+	"github.com/webx-top/com"
+)
+
+func TestForms(t *testing.T) {
+	type Data struct {
+		Test string
+	}
+	mp := map[string]interface{}{
+		`name`: `test`,
+		`age`:  20,
+		`items`: map[string]string{
+			`itemK1`: `itemV1`,
+		},
+		`data`: &Data{
+			Test: `test-data`,
+		},
+		`list`: []string{
+			`1`, `2`,
+		},
+		`listData`: []*Data{
+			&Data{
+				Test: `test-listdata-1`,
+			}, &Data{
+				Test: `test-listdata-2`,
+			},
+		},
+	}
+	cfg := NewConfig()
+	cfg.AddElement(&config.Element{
+		ID:    `input-name`,
+		Type:  `text`,
+		Name:  `name`,
+		Label: `名称`,
+	}, &config.Element{
+		ID:    `input-items-k1`,
+		Type:  `text`,
+		Name:  `items.itemK1`,
+		Label: `Item K1`,
+	}, &config.Element{
+		ID:    `input-data-test`,
+		Type:  `text`,
+		Name:  `data.test`,
+		Label: `Data`,
+	}, &config.Element{
+		ID:    `input-list-0`,
+		Type:  `text`,
+		Name:  `list.0`,
+		Label: `List 0`,
+	}, &config.Element{
+		ID:    `input-list-2`,
+		Type:  `text`,
+		Name:  `list.2`,
+		Label: `List 2`,
+	}, &config.Element{
+		ID:    `input-listdata-0`,
+		Type:  `text`,
+		Name:  `listData.0.test`,
+		Label: `ListData 0`,
+	})
+	form := NewWithModelConfig(mp, cfg)
+	com.Dump(form.Data())
+	result := form.String()
+	fmt.Println(result)
+}
--- go.mod
+++ go.mod
@@ -6,12 +6,12 @@ require (
 	github.com/admpub/fsnotify v1.5.0 // indirect
 	github.com/admpub/json5 v0.0.1
 	github.com/francoispqt/gojay v1.2.13 // indirect
-	github.com/goccy/go-json v0.7.6 // indirect
-	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/goccy/go-json v0.7.8 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/stretchr/testify v1.7.0
-	github.com/webx-top/com v0.2.7
+	github.com/webx-top/com v0.2.8
 	github.com/webx-top/tagfast v0.0.0-20161020041435-9a2065ce3dd2
 	github.com/webx-top/validation v0.0.3
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
-	golang.org/x/sys v0.0.0-20210820121016-41cdb8703e55 // indirect
+	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
 )
