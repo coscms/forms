10b33070d3779e2c7a4857259a1a943ff716d46d
Hank Shen <swhbox@foxmail.com>
2025-11-26 15:18:40 +0800

    update

--- fields/field.go
+++ fields/field.go
@@ -101,6 +101,11 @@ func (f *Field) SetTemplate(tmpl string, theme ...string) FieldInterface {
 	return f
 }
 
+func (f *Field) SetType(fieldType string) FieldInterface {
+	f.Type = fieldType
+	return f
+}
+
 func (f *Field) ReinitTemplate() FieldInterface {
 	f.widget = widgets.BaseWidget(f.Theme, f.Type, f.Template)
 	return f
