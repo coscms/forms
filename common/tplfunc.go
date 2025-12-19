f80a50ce0871285f869b9c0a49b95089e4ec4424
Hank Shen <swhbox@foxmail.com>
2025-12-19 19:06:43 +0800

    update

--- common/tplfunc.go
+++ common/tplfunc.go
@@ -26,15 +26,20 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/gosimple/slug"
 	"github.com/webx-top/com"
 )
 
 var TplFuncs = func() template.FuncMap {
 	return template.FuncMap{
-		`RandomString`: RandomString,
+		`RandomString`:    RandomString,
+		`Slugify`:         Slugify,
+		`SlugifyMaxWidth`: SlugifyMaxWidth,
 	}
 }
 
+// RandomString 生成一个指定长度的随机字母数字字符串
+// 如果未指定长度，则默认生成8位长度的字符串
 func RandomString(length ...uint) string {
 	if len(length) > 0 && length[0] > 0 {
 		return com.RandomAlphanumeric(length[0])
@@ -42,6 +47,23 @@ func RandomString(length ...uint) string {
 	return com.RandomAlphanumeric(8)
 }
 
+// Slugify 将字符串转换为URL友好的slug格式
+func Slugify(v string) string {
+	return slug.Make(v)
+}
+
+// SlugifyMaxWidth 将字符串转换为slug格式并限制最大长度
+// v: 需要转换的原始字符串
+// maxWidth: 返回字符串的最大长度限制
+// 返回: 转换后的slug字符串，长度不超过maxWidth
+func SlugifyMaxWidth(v string, maxWidth int) string {
+	return com.Substr(slug.Make(v), ``, maxWidth)
+}
+
+// ParseFiles 从给定的文件路径中解析模板文件并返回一个模板对象。
+// 如果配置了文件系统(FileSystem)，则使用ParseFS进行解析，否则从本地文件系统读取。
+// 第一个文件将作为主模板，其余文件将作为附加模板进行解析。
+// 返回解析后的模板对象和可能发生的错误。
 func ParseFiles(files ...string) (*template.Template, error) {
 	if !FileSystem.IsEmpty() {
 		return ParseFS(FileSystem, files...)
@@ -60,6 +82,17 @@ func ParseFiles(files ...string) (*template.Template, error) {
 	return tmpl, err
 }
 
+// ParseFS 从给定的文件系统(fs)中解析指定的模板文件，并返回一个模板对象。
+// 第一个文件将作为主模板，后续文件将作为子模板解析。
+// 参数：
+//
+//	fs: 文件系统接口
+//	files: 要解析的模板文件路径列表
+//
+// 返回值：
+//
+//	*template.Template: 解析后的模板对象
+//	error: 解析过程中遇到的错误
 func ParseFS(fs fs.FS, files ...string) (*template.Template, error) {
 	name := filepath.Base(files[0])
 	tmpl := template.New(name)
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ toolchain go1.24.1
 
 require (
 	github.com/admpub/json5 v0.0.1
+	github.com/gosimple/slug v1.15.0
 	github.com/stretchr/testify v1.9.0
 	github.com/webx-top/com v1.4.1
 	github.com/webx-top/tagfast v0.0.1
@@ -18,6 +19,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/francoispqt/gojay v1.2.13 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/hashicorp/go-version v1.8.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
